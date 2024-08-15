565c911d9abab949c64067d9e4c75a6b4814886d
rlarldud1234 <rlarldud6482@naver.com>
2024-08-15 18:39:35 +0900

    new : User DTO

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,55 @@
+import {
+  IsNumber,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class UserInfoDTO {
+  @IsString()
+  id: string;
+
+  @IsString()
+  username: string;
+}
+
+export class CreateUserDTO {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  id: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(16)
+  @Matches(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$/, {
+    message:
+      '패스워드는 8~20자리이며 최소 하나 이상의 영문자, 최소 하나 이상의 숫자, 최소 하나 이상의 특수문자를 입력해야 합니다.',
+  })
+  password: string;
+}
+
+export class SignInUserDTO {
+  @IsString()
+  id: string;
+
+  @IsString()
+  password: string;
+}
+
+export class UserTokenDTO {
+  @IsNumber()
+  user_id: number;
+
+  @IsString()
+  access_token: string;
+
+  @IsString()
+  refresh_token: string;
+}
