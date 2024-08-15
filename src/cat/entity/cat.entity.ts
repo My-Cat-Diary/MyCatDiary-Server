0dc9acb82d32374cd8f80a1df4ccd48dc03c012a
rlarldud1234 <rlarldud6482@naver.com>
2024-08-15 18:38:40 +0900

    new : Cat Entity

---
+++ src/cat/entity/cat.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { SexType } from './type/cat.sex-type';
+
+@Entity({ name: 'cat' })
+export class CatEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  name: string;
+
+  @Column({ default: 'NONE' })
+  sex: SexType;
+
+  @Column()
+  age: number;
+
+  @Column({ name: 'birth_day' })
+  birthDay: Date;
+
+  @Column({ name: 'user_id' })
+  userId: number;
+}
