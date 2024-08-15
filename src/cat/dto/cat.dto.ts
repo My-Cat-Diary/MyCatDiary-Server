cf2b35d12cf6990feda52e3650c961eb299860c4
rlarldud1234 <rlarldud6482@naver.com>
2024-08-15 18:38:33 +0900

    new : Cat DTO

---
+++ src/cat/dto/cat.dto.ts
@@ -0,0 +1,38 @@
+import {
+  IsDate,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { SexType } from '../entity/type/cat.sex-type';
+
+export class CreateCatDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(1)
+  @MaxLength(30)
+  name: string;
+
+  @IsEnum(SexType)
+  sex: SexType;
+
+  @IsNumber()
+  age: number;
+
+  birth_day: Date;
+}
+
+export class CatResDTO {
+  cat_id: number;
+
+  name: string;
+
+  sex: SexType;
+
+  age: number;
+
+  birth_day: Date;
+}
