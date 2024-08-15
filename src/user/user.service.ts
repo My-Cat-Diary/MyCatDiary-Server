a01118409ebfc1b74057da98a4495d14e3b7d9fc
rlarldud1234 <rlarldud6482@naver.com>
2024-08-15 19:06:01 +0900

    new : userService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { CatRepository } from 'src/cat/cat.repository';
+import { UserEntity } from './entity/user.entity';
+import { UserAndCatInfoDTO } from './dto/user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly catRepository: CatRepository,
+  ) {}
+
+  async fetchUserAndCatInfo(user: UserEntity): Promise<UserAndCatInfoDTO> {
+    const owner = await this.userRepository.getUserById(user.id);
+    const cats = await this.catRepository.getCatList(user.userId);
+
+    if (owner) {
+      return {
+        user: {
+          id: owner.id,
+          username: owner.username,
+        },
+        cats: cats.map((cat) => ({
+          cat_id: cat.id,
+          name: cat.name,
+          age: cat.age,
+          sex: cat.sex,
+          birth_day: cat.birthDay,
+        })),
+      };
+    } else {
+      throw new NotFoundException();
+    }
+  }
+}
