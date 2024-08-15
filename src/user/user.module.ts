e57bb17c4a17e1d311ba31f601f015c045204c22
rlarldud1234 <rlarldud6482@naver.com>
2024-08-15 19:05:36 +0900

    update : user module 적용

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,10 +4,12 @@ import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
 import { UserRepository } from './user.repository';
+import { CatRepository } from 'src/cat/cat.repository';
+import { CatEntity } from 'src/cat/entity/cat.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, CatEntity])],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, CatRepository],
 })
 export class UserModule {}
