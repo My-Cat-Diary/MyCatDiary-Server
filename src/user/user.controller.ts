a1854118423b58eb410928ef6d6fcd2ef80bce2c
rlarldud1234 <rlarldud6482@naver.com>
2024-08-15 19:05:10 +0900

    update : fetchUserAndCatInfo

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { UserAndCatInfoDTO } from './dto/user.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './entity/user.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('/info')
+  @UseGuards(AuthGuard('jwt'))
+  async fetchUserAndCatInfo(@Req() req): Promise<UserAndCatInfoDTO> {
+    const userAndInfo = await this.userService.fetchUserAndCatInfo(
+      req.user as UserEntity,
+    );
+
+    return userAndInfo;
+  }
+}
